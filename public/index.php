5ab066ef857f56b45bab14f03ae1e02d9efb3b2e
Isaac <isaac@ktbo.mx>
2019-02-26 15:30:51 -0600

    Change

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@
  */
 
 define('LARAVEL_START', microtime(true));
-/*
+
 header('Access-Control-Allow-Origin:  *');
 header('Access-Control-Max-Age:  1000');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
 header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
-*/
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
